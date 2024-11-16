c5afd0a3f26d55fc8ef6cf45c9879ed0e0b394f3
said antonio valencia castrillo <desarrollador4@bascosta.com>
2024-11-16 10:58:24 -0500

    productList and OrderList components

---
+++ src/components/OrderList.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="orders-list-container">
+        <h1>Orders List</h1>
+        <table>
+            <thead>
+                <tr>Order number</tr>
+                <tr>Total</tr>
+                <tr>Actions</tr>
+            </thead>
+            <tbody>
+                <tr v-for="order in orders" :key="order.id"></tr>
+                <td>{{ order.orderNumber }}</td>
+                <td>{{ order.total }}</td>
+                <td>
+                    <button class="btn-small" @click="editOrder(order.id)">Edit</button>
+                    <button class="btn-small btn-danger" @click="deleteOrder(order.id)">Delete</button>
+                </td>
+            </tbody>
+        </table>
+        <router-link to="/create-order" class="add-order-link">Add New Order</router-link>
+    </div>
+</template>
+
+<script>
+import {db} from '../firebaseConfig.js'
+import {collection, addDoc,getDocs,doc, deleteDoc,updateDoc} from 'firebase/firestore'
+
+export default{
+    data(){
+        return{
+            orders : [],
+        }
+    },
+    async created(){
+        await this.fetchOrders();
+    },
+    methods:{
+        async fetchOrders(){
+            const querySnapshot =await getDocs(collection(db, 'orders'))
+            this.orders = querySnapshot.docs
+        },
+        editOrder(id){
+
+        },
+        async deleteOrder(id){
+            await deleteDoc(doc(db,'orders', id))
+            await this.fetchOrders()
+
+        },
+    }
+}
+</script>
--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -1,20 +1,34 @@
 <template>
-    <div class="product-list-container"></div>
-    <h1>Product List</h1>
-    <table>
-        <thead>
-            <tr>Name</tr>
-            <tr>Price</tr>
-            <tr>Descriptions</tr>
-            <tr>Actions</tr>
-        </thead>
-        <tbody>
-            <tr v-for="product in products" :key="product.id"></tr>
-            <td>{{ product.name }}</td>
-             <td>{{ product.price }}</td>
-             <td>{{ product.description }}</td>
-        </tbody>
-    </table>
+    <div class="product-list-container">
+        <h1>Product List</h1>
+        <table>
+            <thead>
+                <tr>Name</tr>
+                <tr>Price</tr>
+                <tr>Descriptions</tr>
+                <tr>Actions</tr>
+            </thead>
+            <tbody>
+                <tr v-for="product in products" :key="product.id"></tr>
+                <td>{{ product.name }}</td>
+                <td>{{ product.price }}</td>
+                <td>{{ product.description }}</td>
+                <td>
+                    <button class="btn-small" @click="editProduct(product.id)">Edit</button>
+                    <button class="btn-small btn-danger" @click="deleteProduct(product.id)">Delete</button>
+                </td>
+            </tbody>
+        </table>
+        <button @click="showAddProduct == true">Add new Product</button>
+        <div v-if="showAddProduct || selectedProduct"m class="product-form">
+            <input v-model="productForm.name" placeholder="Nombre">
+            <input v-model="productForm.price" placeholder="Precio" type="number">
+            <textarea v-model="productForm.description" placeholder="Descripción"></textarea>
+
+            <button @click="submitProduct">submit</button>
+            <button @click="cancel">Cancel</button>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -26,6 +40,7 @@ export default{
         return{
             products : [],
             showAddProduct : false,
+            selectedProduct : null,
             productForm: {
                 name: '',
                 price: 0,
@@ -33,5 +48,43 @@ export default{
             }
         }
     },
+    async created(){
+        await this.fetchProducts();
+    },
+    methods:{
+        async fetchProducts(){
+            const querySnapshot =await getDocs(collection(db, 'products'))
+            this.products = querySnapshot.docs
+            .map(doc => ({id: doc.id, ...doc.data()}))
+            .sort((a,b)=> a.name.localCompare(b.name))
+        },
+        editProduct(id){
+            const product = this.product.find(product => product.id == id)
+            this.productForm = {...product}
+            this.selectedProduct = id
+            this.showAddProduct = true
+
+        },
+        async deleteProduct(id){
+            await deleteDoc(doc(db,'products', id))
+            await this.fetchProducts()
+
+        },
+        async submitProduct(){
+            if(this.selectedProduct){
+                await updateDoc(doc(db,'products', this.selectedProduct), this.productForm)
+            }else{
+                addDoc(collection(db,'products'),this.productForm)
+            }
+            this.cancel()
+            await this.fetchProducts()
+
+        }, cancel(){
+            this.showAddProduct = false
+            this.selectedProduct = null
+            this.productForm = {name: '', price:0, description: ''}
+
+        }
+    }
 }
 </script>
